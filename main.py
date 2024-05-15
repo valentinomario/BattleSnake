931fbf0625c04d11ae1a490d9ae509cfb441c489
Mario Valentino <valentinomario05@gmail.com>
2024-05-15 13:10:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -124,10 +124,13 @@ def search_closest_safest_food(game_state: typing.Dict):
     return None
 
 def move (game_state: typing.Dict) -> typing.Dict:
+    printly(game_state, "Move: " + game_state["turn"])
     processedBoard = defineBoard(game_state)
     safe_moves = safeMove(game_state, processedBoard)
     selected_move = miniMax_value(game_state, safe_moves)
     return {"move": selected_move}
+
+
 def defineBoard(game_state: typing.Dict):
     # Create grid for A*
     height = game_state["board"]["height"]  # rows
